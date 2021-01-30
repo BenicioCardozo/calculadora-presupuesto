6308eaa87ecd940b97901b33058b69e5946c813b
Benicio Cardozo <beniciocardozomdp@gmail.com>
2021-01-29 21:55:55 -0300

    Refactor Navbar Component

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,75 +1,32 @@
 <template>
-  <b-navbar toggleable type="dark" variant="dark">
-    <span id="top">
-      <b-nav-item href="#">
-        <router-link to="/pedidos">Pedidos</router-link>
-      </b-nav-item>
-      <b-nav-item href="#">
-        <router-link to="/kits">Kits</router-link>
-      </b-nav-item>
-      <b-nav-item href="#">
-        <router-link to="/clientes">Clientes</router-link>
-      </b-nav-item>
-      <b-nav-item href="#">
-        <router-link to="/productos">Productos</router-link>
-      </b-nav-item>
-      <b-nav-item href="#">
-        <router-link to="/materias-primas">Materias Primas</router-link>
-      </b-nav-item>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-button
-        size="sm"
-        style="margin: .3vh 0 0 5vw;"
-        v-if="!loggedIn"
-        variant="primary"
-        @click="login"
-        >Iniciar Sesión</b-button
-      >
-      <b-button
-        size="sm"
-        style="margin: .3vh 0 0 5vw;"
-        v-else
-        variant="primary"
-        @click="logout"
-        >Salir</b-button
-      >
-    </span>
-    <span id="mobile-header">
-      <b-button v-if="!loggedIn" variant="primary" @click="login" size="sm"
-        >Iniciar Sesión</b-button
-      >
-      <b-button v-else variant="primary" @click="logout" size="sm"
-        >Salir</b-button
-      >
-      <b-navbar-toggle
-        id="toggle"
-        #default="{ expanded }"
-        target="navbar-toggle-collapse"
-      >
-        <template>
-          <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
-          <b-icon v-else icon="chevron-bar-down"></b-icon>
-        </template>
-      </b-navbar-toggle>
-    </span>
-
-    <b-collapse id="navbar-toggle-collapse" is-nav>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item href="#">
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav style="width: 100%" class="justify-content-around">
+        <b-nav-item href="#" class="m-auto">
           <router-link to="/pedidos">Pedidos</router-link>
         </b-nav-item>
-        <b-nav-item href="#">
+        <b-nav-item href="#" class="m-auto">
           <router-link to="/kits">Kits</router-link>
         </b-nav-item>
-        <b-nav-item href="#">
+        <b-nav-item href="#" class="m-auto">
           <router-link to="/clientes">Clientes</router-link>
         </b-nav-item>
-        <b-nav-item href="#">
+        <b-nav-item href="#" class="m-auto">
           <router-link to="/productos">Productos</router-link>
         </b-nav-item>
-        <b-nav-item href="#">
+        <b-nav-item href="#" class="m-auto">
           <router-link to="/materias-primas">Materias Primas</router-link>
         </b-nav-item>
+        <b-nav-form class="m-auto login-buttons">
+          <b-button v-if="!loggedIn" variant="primary" @click="login"
+            >Iniciar Sesión</b-button
+          >
+          <b-button v-else variant="primary" @click="logout"
+            >Salir</b-button
+          ></b-nav-form
+        >
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
@@ -82,51 +39,16 @@
 </script>
 
 <style scoped>
-  #top {
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    width: 92%;
-    list-style: none;
-    flex-wrap: wrap;
+  a {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     font-size: 1.2em;
-  }
-  #mobile-header {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-  }
-  a {
+    color: #489cf7 !important;
     padding: 5px;
   }
-  a[href] :hover {
-    text-decoration: none !important ;
-    color: #007bff;
-  }
-  a:hover {
+
+  li :not(.form-inline) :hover {
     background-color: #54268f;
     border-radius: 10px;
-  }
-  #top a.router-link-exact-active {
-    color: #42b983;
-  }
-  @media screen and (min-width: 750px) {
-    #mobile-header {
-      display: none;
-    }
-  }
-  @media screen and (max-width: 750px) {
-    #top {
-      display: none;
-    }
-    #navbar-toggle-collapse > * {
-      text-align: center;
-      font-size: 1.2em;
-    }
-    .navbar {
-      display: flex;
-      justify-content: flex-end !important;
-    }
+    text-decoration: none !important ;
   }
 </style>
